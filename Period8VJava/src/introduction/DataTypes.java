e48e4299cf093e33ba9617e33f10c6c78850a4b7
veera <veera@DESKTOP-0ECFG49>
2016-09-19 22:08:16 -0400

    homework

--- Period8VJava/src/introduction/DataTypes.java
+++ Period8VJava/src/introduction/DataTypes.java
@@ -4,8 +4,55 @@ public class DataTypes {
 
 	public static void main(String[] args) {
 		//System.out.println((int)(3.0 + 5)/(2 + 5 * 2));
-		if(42 == 42.0) {
-			System.out.println("42 equals 42.0");
-		}
+		/*double d1 = 4.64; 
+		double d2 = 2.0;
+		double d3 = 2.64;
+
+		System.out.println("d1 : " + d1); 
+		System.out.println("d2 : " + d2); 
+		System.out.println("d3 : " + d3);  
+		System.out.println("d1 - d2 : " + (d1 - d2));*/
+		
+		//1 cannot convert from string to int
+		//String s = 1;
+		//System.out.println(s);
+		
+		//2 cannot cast from int to string
+		//String s = (String)1;
+		//System.out.println(s);
+		
+		//3 prints 1
+		//String s = "1";
+		//System.out.println(s);
+		
+		//4 prints 1
+		//String s = ""+1;
+		//System.out.println(s);
+		
+		//5 prints "Happy 18th Birthday"
+		//String s = "Happy "+18+"th birthday!";
+		//System.out.println(s);
+		
+		//6 prints "Happy 18th Birthday!"
+		//String s = "Happy 18th birthday!";
+		//System.out.println(s);
+		
+		//7 prints "Happy 18th birthday!"
+		//String s = "Happy "+54/3+"th birthday!";
+		//System.out.println(s);
+		
+		//8 prints "Happy 18th birthday!"
+		//String s = "Happy "+(54/3)+"th birthday!";
+		//System.out.println(s);
+		
+		//9 prints "m/n is 3" 
+		//int m = 22;
+		//int n = 7;
+		//System.out.println("m/n is "+(m/n));
+		
+		//10 prints "m/n is 3.142857142857143"
+		int m = 22;
+		int n = 7;
+		System.out.println("m/n is "+((double)m/n));
 	}
 }
