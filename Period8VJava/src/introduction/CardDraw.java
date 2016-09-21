389a36aa47c8d11ea8ee37bc5303a27a0fec288a
veera <veera@DESKTOP-0ECFG49>
2016-09-20 22:59:47 -0400

    Homework Card Draw

---
+++ Period8VJava/src/introduction/CardDraw.java
@@ -0,0 +1,16 @@
+package introduction;
+
+public class CardDraw {
+
+	public static void main(String[] args) {
+		String[] values = {"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", 
+						   "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+		String[] suits = {"Spades", "Hearts", "Clubs", "Diamonds"};
+		int card = (int) (Math.random() *  values.length) + 0;
+		
+		int card2 = (int) (Math.random() * suits.length) + 0;
+		
+		String name = ("You just picked the " + values[card] + " of " + suits[card2]);
+		System.out.println(name);
+	}
+}
