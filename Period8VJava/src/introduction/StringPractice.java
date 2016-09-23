ece746a648ccef6c06bd365ee69c054d87d07668
Veeraj <Student 8@10.8.33.160>
2016-09-23 14:02:50 -0400

    In Class 9/23/16

--- Period8VJava/src/introduction/StringPractice.java
+++ Period8VJava/src/introduction/StringPractice.java
@@ -59,15 +59,52 @@ public class StringPractice {
 			print("word1 comes before word2");
 	}
 	
-	public static void print(String s){
+	 public static void print(String s){
+		 String printString = "";
+		 int cutoff = 25;
+		 //this while loop last as long as there are words left in the original String
+		 while(s.length() > 0){
+
+		 String currentCut = "";
+		 String nextWord = "";
+
+		 //while the current cut is still less than the line length 
+		 //AND there are still words left to add
+		 while(currentCut.length()+nextWord.length() < cutoff && s.length() > 0){
+
+		 //add the next word
+		 currentCut += nextWord;
+		 
+		 //remove the word that was added from the original String
+		 s = s.substring(nextWord.length());
+		 
+		 //identify the following word, exclude the space
+		 int endOfWord = s.indexOf(" ");
+
+		 //if there are no more spaces, this is the last word, so add the whole thing
+		 if(endOfWord == -1) {
+		 endOfWord = s.length()-1;//subtract 1 because index of last letter is one les than length
+		 }
+
+		 //the next word should include the space
+		 nextWord = s.substring(0,endOfWord+1);
+		 }
+
+		 printString +=currentCut+"\n";
+
+		 }
+		 System.out.print(printString);
+		 }
+
+	/*public static void print(String s){
 		String printString = s;
 		int cutoff = 35;
 		//Check for words to add (s has a length > 0)
 		while(s.length() > 0){
 			String cut = "";
 			String nextWord = "";
-		/* Check to see if the next word will fit on the line and there must still be words
-		to add */
+		Check to see if the next word will fit on the line and there must still be words
+		to add
 			while(cut.length() + nextWord.length() < cutoff && s.length() > 0){
 				//Add the next word to the line
 				cut += nextWord;
@@ -88,7 +125,8 @@ public class StringPractice {
 		}
 		
 		System.out.println(printString);
-	}
+	}*/
+	
 	/*private static String getCut(String s, int cutoff, int cut){
 		int cutIndex = cut * cutoff;
 		if (cutIndex > s.length()){
