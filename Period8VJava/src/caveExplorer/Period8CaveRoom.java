988ea08ded9a2526aed3a90b673c744a6b90fd13
Veeraj <Student 8@10.8.33.160>
2016-10-31 14:03:04 -0400

    Setting Up Project

---
+++ Period8VJava/src/caveExplorer/Period8CaveRoom.java
@@ -0,0 +1,16 @@
+package caveExplorer;
+
+public class Period8CaveRoom {
+
+	public Period8CaveRoom(String string) {
+
+	}
+
+	public void enter() {
+		
+	}
+
+	public void setConnection(Period8CaveRoom period8CaveRoom) {
+		
+	}
+}
