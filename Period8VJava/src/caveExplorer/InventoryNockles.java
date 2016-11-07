d2c9113c03274f16faf4eda5c314894701ef4503
veeraj-jethalal <vjethalal8416@bths.edu>
2016-11-07 12:47:51 -0500

    Update InventoryNockles.java

--- Period8VJava/src/caveExplorer/InventoryNockles.java
+++ Period8VJava/src/caveExplorer/InventoryNockles.java
@@ -12,6 +12,21 @@ public class InventoryNockles {
 
 	private void updateMap() {
 		CaveRoomPd8[][] caves = CaveExplorer.caves;
+		//create the top side
+		map = " ";
+		//horizontal line across top row width 4
+		//except for the last one
+		for(int i = 0; i < caves[0].length-1; i++){
+			map += "____";
+		}
+		map+="___\n";
+		//each room
+		for(CaveRoomPd8[] row: caves){
+			//There are three rows of text
+			for(int textRow = 0; textRow < 3; textRow++){
+				//The text is for each room	
+			}
+		}
 	}
 
 	public String getDescription() {
