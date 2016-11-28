1042b82a902f7b3e1c4b64e27796c91b28a53771
Veeraj <Period82.0@10.8.33.160>
2016-11-28 14:00:32 -0500

    fg

--- Period8VJava/src/sort/PokemonClass.java
+++ Period8VJava/src/sort/PokemonClass.java
@@ -9,38 +9,46 @@ public class PokemonClass {
 	public static void main(String[] args){
 		PokemonClass squirtle = new PokemonClass("Squirtle", 26);
 		PokemonClass bulbasaur = new PokemonClass("Bulbasaur", 50);
+		squirtle.iChooseYou("Squirtle");
+		bulbasaur.iChooseYou("Bulbasaur");
 	}
-	public PokemonClass(String name2, int level2){
-		name = name2;
-		level = level2;
-		poisoned = false;
+	
+	public PokemonClass(String name, int level){
+		this.name = name;
+		this.level = level;
+		this.poisoned = false;
 		hp = 100;
 	}
 	
+	public void attack(PokemonClass target, Attack attack){
+		if(Math.random() < .9){
+			attack.attack(target);
+			System.out.println("The attack hit");
+		}
+		else System.out.println("The attack missed");
+	}
+	
 	public void iChooseYou(String name){
-		System.out.println(name);
-		System.out.println(name);
+		System.out.println(name + ", " + name + "!");
 	}
 	
 	public int getHP(){
-		return 0;
+		return hp;
 	}
 	
 	public String getName(){
-		return "";
+		return name;
 	}
 	
 	public void setHP(int newHP){
-		
+		hp = newHP;
 	}
 	
 	public void setPoisoned(boolean b){
-		
+		poisoned = b;
 	}
 	
 	public void lapse(){
-		if(poisoned){
-			hp = hp - 15;
-		}
+		if(poisoned)hp-=15;
 	}
 }
