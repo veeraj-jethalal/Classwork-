08b468b10185a77f4fff1ba90221e92239008b98
Veeraj <Period82.0@10.8.33.160>
2016-11-29 13:00:41 -0500

    Pokemon

--- Period8VJava/src/sort/MainBattle.java
+++ Period8VJava/src/sort/MainBattle.java
@@ -1,7 +1,7 @@
 package sort;
 
 public class MainBattle {
-
+//
 	public static void main(String[] args){
 		Pokemon squirtle = new Pokemon("Squirtle", 26);
 		Pokemon bulbasaur = new Pokemon("Bulbasaur", 50);
