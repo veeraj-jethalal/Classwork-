2c306a34492001e24266ea9ded91cd94933c9765
Veeraj <Period82.0@10.8.33.160>
2016-11-29 13:00:19 -0500

    Pokemon

--- Period8VJava/src/sort/MainBattle.java
+++ Period8VJava/src/sort/MainBattle.java
@@ -9,7 +9,25 @@ public class MainBattle {
 		bulbasaur.iChooseYou("Bulbasaur");
 		System.out.println("Squirtle is preparing to " +
 				"attack with water blast");
-		squirtle.attack(bulbasaur, attack);
-		//Anonymous Inner Type
+		squirtle.attack(bulbasaur, new Attack(){
+			public void attack(Pokemon target){
+				//cuts health in half
+				int newHP = target.getHP()/2;
+				target.setHP(newHP);
+			}
+		});
+		bulbasaur.attack(squirtle, new Attack() {
+			public void attack(Pokemon target) {
+				target.setPoisoned(true);
+			}
+		});
+		squirtle.lapse();
+		bulbasaur.lapse();
+		printScore(squirtle, bulbasaur);
+	}
+	
+	public static void printScore(Pokemon p1, Pokemon p2){
+		System.out.println(p1.getName() + ", HP = " + p1.getHP());
+		System.out.println(p2.getName() + ", HP = " + p2.getHP());
 	}
 }
