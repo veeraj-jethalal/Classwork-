79d98936c5700edc01e4c405518d8ad09d6445d6
Veeraj <Period82.0@10.8.33.160>
2016-12-08 14:03:55 -0500

    d

--- Period8VJava/src/guiPractice/Screen.java
+++ Period8VJava/src/guiPractice/Screen.java
@@ -3,6 +3,7 @@ package guiPractice;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 public class Screen {
@@ -23,12 +24,23 @@ public class Screen {
 		update();
 	}
 
-	public void update(){
+	public void update(){ 
 		//this is where you draw stuff
 		Graphics2D g = image.createGraphics();
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 		g.setColor(Color.black);
-		g.setFont(new Font("Helvetica" , Font.PLAIN, 50));
-		g.drawString("Hello", 100, 100);
+		g.setFont(new Font("Helvetica" , Font.BOLD, 20));
+		g.drawString("Hello", 57, 85);
+		g.drawOval(40, 40, 80, 80);
+		g.drawRect(40, 120, 80, 100);
+		g.drawLine(50, 220, 40, 265);
+		g.drawLine(90, 220, 100, 265);
+		
+		g.setColor(Color.green);
+		for(int i = 0; i < 200; i+=2){
+			g.drawLine(i, 265, i, 280);
+		}
 	}
 	
 	public BufferedImage getImage(){
