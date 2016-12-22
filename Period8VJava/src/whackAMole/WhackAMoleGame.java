ba65f67d631fcd8aebf352bd675214adeb8bd737
Veeraj <Period82.0@10.8.33.160>
2016-12-22 14:01:12 -0500

    d

--- Period8VJava/src/whackAMole/WhackAMoleGame.java
+++ Period8VJava/src/whackAMole/WhackAMoleGame.java
@@ -4,16 +4,18 @@ import guiPractice.GUIApplication;
 
 public class WhackAMoleGame extends GUIApplication {
 
+	public static void main(String[] args) {
+		WhackAMoleGame game = new WhackAMoleGame();
+		Thread app = new Thread(game);
+		app.start();
+	}
+
 	public WhackAMoleGame() {
 	}
 
 	@Override
 	protected void initScreen() {
-
+		WhackAMoleScreen wams = new WhackAMoleScreen(getWidth(), getHeight());
+		setScreen(wams);		
 	}
-
-	public static void main(String[] args) {
-
-	}
-
 }
